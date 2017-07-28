bdc2d49a9212f1ea837eeaf3865188d6319f8c18
Miikka Virtanen <dragory.up@gmail.com>
2017-07-28 22:50:09 +0300

    Initial commit

---
+++ src/BasePlugin.ts
@@ -0,0 +1,11 @@
+import {Client} from 'eris';
+
+export class BasePlugin {
+    protected bot: Client;
+    protected pluginConfig: object;
+
+    constructor(bot: Client, pluginConfig: object) {
+        this.bot = bot;
+        this.pluginConfig = pluginConfig;
+    }
+}
