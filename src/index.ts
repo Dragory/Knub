4609db1000dfb4047c93daac00283ce3c1d80bce
Miikka Virtanen <dragory.up@gmail.com>
2017-08-24 22:29:48 +0300

    Update export name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { BotFramework } from "./BotFramework";
 
 // Include both, a named and default export
 export default BotFramework;
-export { BotFramework as Hermes };
+export { BotFramework as Knub };
 
 export { IConfigProvider } from "./IConfigProvider";
 export { Plugin } from "./Plugin";
