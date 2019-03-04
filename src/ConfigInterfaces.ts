de56a186312eedb27cc0d94d93f8aa46d04ee016
Dragory <dragory.up@gmail.com>
2019-03-04 20:53:46 +0200

    Fix overrides with modifiers being marked as invalid when using config/permission types

--- src/ConfigInterfaces.ts
+++ src/ConfigInterfaces.ts
@@ -1,5 +1,28 @@
 import { DeepPartial } from "ts-essentials";
 
+export type WithoutProps<T> = { [P in keyof T]: never };
+
+// Heavily based on DeepPartial from ts-essentials
+export type DeepPartialOrUnknownA<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepPartialOrUnknown<U>>
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartialOrUnknown<U>>
+    : DeepPartialOrUnknown<T[P]>
+};
+
+export interface IDeepPartialOrUnknownB {
+  [key: string]: any;
+}
+
+/**
+ * Recursive object type where the object can have any properties, but properties from T are type checked
+ * while unknown properties can be anything.
+ */
+export type DeepPartialOrUnknown<T> =
+  | DeepPartialOrUnknownA<T>
+  | (IDeepPartialOrUnknownB & WithoutProps<T>);
+
 export interface IPermissionLevelDefinitions {
   [roleOrUserId: string]: number;
 }
@@ -26,7 +49,10 @@ export interface IGlobalConfig {
   [key: string]: any;
 }
 
-export interface IPartialPluginOptions<TConfig = IBasePluginConfig, TPermissions = IBasePluginPermissions> {
+export interface IPartialPluginOptions<
+  TConfig = IBasePluginConfig,
+  TPermissions = IBasePluginPermissions
+> {
   enabled?: boolean;
   permissions?: DeepPartial<TPermissions>;
   config?: DeepPartial<TConfig>;
@@ -34,20 +60,30 @@ export interface IPartialPluginOptions<TConfig = IBasePluginConfig, TPermissions
   "=overrides"?: Array<IPluginConfigOverride<TConfig, TPermissions>>;
 }
 
-export interface IPluginOptions<TConfig = IBasePluginConfig, TPermissions = IBasePluginPermissions>
-  extends IPartialPluginOptions<TConfig, TPermissions> {
+export interface IPluginOptions<
+  TConfig = IBasePluginConfig,
+  TPermissions = IBasePluginPermissions
+> extends IPartialPluginOptions<TConfig, TPermissions> {
   permissions: TPermissions;
   config: TConfig;
 }
 
-export interface IPluginConfigOverride<TConfig = IBasePluginConfig, TPermissions = IBasePluginPermissions> {
+export interface IPluginConfigOverride<
+  TConfig = IBasePluginConfig,
+  TPermissions = IBasePluginPermissions
+> {
   channel?: string | string[];
   level?: string | string[];
   user?: string | string[];
   role?: string | string[];
   type?: "any" | "all";
-  config?: DeepPartial<TConfig>;
-  permissions?: DeepPartial<TPermissions>;
+
+  // We use DeepPartialOrUnknown here so that properties with modifiers (e.g. "+" or "-" for arrays) are allowed
+  // while the actual defined config/permission properties are still type checked. It's not a perfect solution since
+  // these properties with modifiers are not type checked (statically), but it's a decent compromise for now.
+  // Can hopefully eventually be solved by this issue: https://github.com/Microsoft/TypeScript/issues/12754
+  config?: DeepPartialOrUnknown<TConfig>;
+  permissions?: DeepPartialOrUnknown<TPermissions>;
 }
 
 export interface IBasePluginPermissions {
