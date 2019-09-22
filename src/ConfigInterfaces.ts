48cc86d7f54b0c075750e74e02901a6655db49b9
Dragory <2606411+Dragory@users.noreply.github.com>
2019-09-22 14:17:31 +0300

    Remove now-unneeded modifier-related typing from config interfaces

--- src/ConfigInterfaces.ts
+++ src/ConfigInterfaces.ts
@@ -1,26 +1,5 @@
 import { DeepPartial } from "ts-essentials";
 
-export type WithoutProps<T> = { [P in keyof T]: never };
-
-// Heavily based on DeepPartial from ts-essentials
-export type DeepPartialOrUnknownA<T> = {
-  [P in keyof T]?: T[P] extends Array<infer U>
-    ? Array<DeepPartialOrUnknown<U>>
-    : T[P] extends ReadonlyArray<infer U>
-    ? ReadonlyArray<DeepPartialOrUnknown<U>>
-    : DeepPartialOrUnknown<T[P]>
-};
-
-export interface IDeepPartialOrUnknownB {
-  [key: string]: any;
-}
-
-/**
- * Recursive object type where the object can have any properties, but properties from T are type checked
- * while unknown properties can be anything.
- */
-export type DeepPartialOrUnknown<T> = DeepPartialOrUnknownA<T> | (IDeepPartialOrUnknownB & WithoutProps<T>);
-
 export interface IPermissionLevelDefinitions {
   [roleOrUserId: string]: number;
 }
@@ -65,12 +44,7 @@ export interface IPluginConfigOverride<TConfig = IBasePluginConfig> {
   user?: string | string[];
   role?: string | string[];
   type?: "any" | "all";
-
-  // We use DeepPartialOrUnknown here so that properties with modifiers (e.g. "+" or "-" for arrays) are allowed
-  // while the actual defined config/permission properties are still type checked. It's not a perfect solution since
-  // these properties with modifiers are not type checked (statically), but it's a decent compromise for now.
-  // Can hopefully eventually be solved by this issue: https://github.com/Microsoft/TypeScript/issues/12754
-  config?: DeepPartialOrUnknown<TConfig>;
+  config?: DeepPartial<TConfig>;
 }
 
 export interface IBasePluginConfig {
