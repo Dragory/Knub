0ea2e4aca36193a04056549b28db2ede29c77f16
Dragory <2606411+Dragory@users.noreply.github.com>
2020-07-05 00:11:11 +0300

    Improve eventListener() type inference, refactor signature to be cleaner

--- src/events/EventListenerBlueprint.test.ts
+++ src/events/EventListenerBlueprint.test.ts
@@ -1,10 +1,70 @@
 import { eventListener } from "./EventListenerBlueprint";
 import { Message } from "eris";
+import { BasePluginType } from "../plugins/pluginTypes";
+import { expect } from "chai";
 
 type AssertEquals<TActual, TExpected> = TActual extends TExpected ? true : false;
 
-// Test type inference
-eventListener("messageCreate", (args) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const result: AssertEquals<typeof args, { message: Message }> = true;
+describe("eventListener() helper", () => {
+  it("(event, listener)", () => {
+    const blueprint = eventListener("messageCreate", (args) => {
+      // Test type inference
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result: AssertEquals<typeof args, { message: Message }> = true;
+    });
+
+    expect(blueprint.event).to.equal("messageCreate");
+    expect(blueprint.listener).to.not.equal(undefined);
+    expect(blueprint.opts).to.equal(undefined);
+  });
+
+  it("(event, opts, listener)", () => {
+    const blueprint = eventListener("messageCreate", { allowSelf: true }, (args) => {
+      // Test type inference
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result: AssertEquals<typeof args, { message: Message }> = true;
+    });
+
+    expect(blueprint.event).to.equal("messageCreate");
+    expect(blueprint.listener).to.not.equal(undefined);
+    expect(blueprint.opts).to.eql({ allowSelf: true });
+  });
+
+  interface CustomPluginType extends BasePluginType {
+    config: {
+      foo: 5;
+    };
+  }
+
+  it("<TPluginType>()(event, listener)", () => {
+    const blueprint = eventListener<CustomPluginType>()("messageCreate", (args, meta) => {
+      // Test type inference
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result: AssertEquals<typeof args, { message: Message }> = true;
+
+      const foo = meta.pluginData.config.get();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result2: AssertEquals<typeof foo, { foo: 5 }> = true;
+    });
+
+    expect(blueprint.event).to.equal("messageCreate");
+    expect(blueprint.listener).to.not.equal(undefined);
+    expect(blueprint.opts).to.equal(undefined);
+  });
+
+  it("<TPluginType>()(event, options, listener)", () => {
+    const blueprint = eventListener<CustomPluginType>()("messageCreate", { allowSelf: true }, (args, meta) => {
+      // Test type inference
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result: AssertEquals<typeof args, { message: Message }> = true;
+
+      const foo = meta.pluginData.config.get();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result2: AssertEquals<typeof foo, { foo: 5 }> = true;
+    });
+
+    expect(blueprint.event).to.equal("messageCreate");
+    expect(blueprint.listener).to.not.equal(undefined);
+    expect(blueprint.opts).to.eql({ allowSelf: true });
+  });
 });
--- src/events/EventListenerBlueprint.ts
+++ src/events/EventListenerBlueprint.ts
@@ -8,17 +8,71 @@ export interface EventListenerBlueprint<TPluginType extends BasePluginType, TEve
 }
 
 /**
- * Helper function that creates an event listener blueprint.
- * Used for type inference between `event` and the arguments for `listener()`.
+ * Helper function to create an event listener blueprint with just event name and listener function
  */
-export function eventListener<TPluginType extends BasePluginType, TEventName extends string>(
+type EventListenerBlueprintCreatorWithoutOpts<TPluginType extends BasePluginType> = <TEventName extends string>(
   event: TEventName,
-  listener: EventListenerBlueprint<TPluginType, TEventName>["listener"],
-  rest?: Omit<EventListenerBlueprint<TPluginType, TEventName>, "event" | "listener">
-): EventListenerBlueprint<TPluginType, TEventName> {
-  return {
-    event,
-    listener,
-    ...rest,
-  };
+  listener: EventListenerBlueprint<TPluginType, TEventName>["listener"]
+) => EventListenerBlueprint<TPluginType, TEventName>;
+
+/**
+ * Helper function to create an event listener blueprint with event name, options, and a listener function
+ */
+type EventListenerBlueprintCreatorWithOpts<TPluginType extends BasePluginType> = <TEventName extends string>(
+  event: TEventName,
+  options: OnOpts,
+  listener: EventListenerBlueprint<TPluginType, TEventName>["listener"]
+) => EventListenerBlueprint<TPluginType, TEventName>;
+
+type EventListenerCreator<TPluginType extends BasePluginType> = EventListenerBlueprintCreatorWithoutOpts<TPluginType> &
+  EventListenerBlueprintCreatorWithOpts<TPluginType>;
+
+/**
+ * Helper function to create an event listener blueprint. Used for type inference from event name.
+ *
+ * To specify `TPluginType` for additional type hints, use: `eventListener<TPluginType>()(event, listener)`
+ */
+export function eventListener<TEventName extends string>(
+  event: TEventName,
+  listener: EventListenerBlueprint<BasePluginType, TEventName>["listener"]
+): EventListenerBlueprint<BasePluginType, TEventName>;
+
+/**
+ * Helper function to create an event listener blueprint. Used for type inference from event name.
+ *
+ * To specify `TPluginType` for additional type hints, use: `eventListener<TPluginType>()(event, options, listener)`
+ */
+export function eventListener<TEventName extends string>(
+  event: TEventName,
+  options: OnOpts,
+  listener: EventListenerBlueprint<BasePluginType, TEventName>["listener"]
+): EventListenerBlueprint<BasePluginType, TEventName>;
+
+/**
+ * Specify `TPluginType` for type hints and return a function: `eventListener(event, listener, rest?)`
+ */
+export function eventListener<TPluginType extends BasePluginType>(): EventListenerCreator<TPluginType>;
+
+export function eventListener(...args) {
+  if (args.length === 2) {
+    // (event, listener)
+    // Return event listener blueprint
+    const [event, listener] = args;
+    return {
+      event,
+      listener,
+    } as EventListenerBlueprint<BasePluginType>;
+  } else if (args.length === 3) {
+    // (event, options, listener)
+    // Return event listener blueprint
+    const [event, options, listener] = args;
+    return {
+      event,
+      listener,
+      opts: options,
+    };
+  }
+
+  // No arguments, with TPluginType - return self
+  return eventListener as EventListenerCreator<BasePluginType>;
 }
