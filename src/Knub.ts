66ab38968b6a67348d4e368efa87ca9cd07db233
Dragory <2606411+Dragory@users.noreply.github.com>
2021-09-04 22:13:44 +0300

    Test sequential guild loading

--- src/Knub.ts
+++ src/Knub.ts
@@ -473,6 +473,9 @@ export class Knub<
   }
 
   protected getGuildLoadQueue(guildId: Snowflake): Queue {
+    // FIXME: Temporary test
+    guildId = "__shared__";
+
     if (!this.guildLoadQueues.has(guildId)) {
       const queueTimeout = 60 * 5 * 1000; // 5 minutes, should be plenty to allow plugins time to load/unload properly
       this.guildLoadQueues.set(guildId, new Queue(queueTimeout));
